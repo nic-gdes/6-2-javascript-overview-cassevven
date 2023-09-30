8e7c059fb301bec0a223ac406d9e7bd1447e37ce
cassevven <119555884+cassevven@users.noreply.github.com>
2023-09-30 16:53:26 -0700

    overview

--- javascript.js
+++ javascript.js
@@ -1,13 +1,51 @@
 // 1.1 Assignment: Where do you put JavaScript?
+alert("Javascript is easy");
 
 // 1.2 Assignment: Variables
+var john_lastName = "Smith";
+console.log(john_lastName);
+var john_firstName= "John";
+console.log(john_firstName);
+var john_age= 25;
+console.log(john_age);
+var john_country= "UK"
+console.log(john_country);
 
 // 1.3 Assignment: Variable Declaration & Initialization
+var a = "Hello World"; //string//
+console.log(a);
+var b = true; //boolean//
+console.log(b);
+var c = null; //null//
+console.log(c);
+var d = 45; //number//
+console.log(d);
+var e; //undefined//
+console.log(e);
 
 // 1.4 Assignment: Complete your statements
+x = 2; y = 3;
+
+z = x + y;
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
+var a = function() {
+    alert('Statement 1');
+   alert('Statement 2');
+}
 
 // 1.6 Assignment: Data Types
+var c = "Charli";
+console.log(c); //string//
+var a = 22;
+console.log(a); //number//
+var s = true //boolean
+console.log(s);
 
 // 1.7 Assignment: Practicing JavaScript basics
+var areaThatPerformsCalculation;
+var a = 3;
+var b = 4;
+var c = 5;
+var area = (a * b / 2);
+console.log(area);
